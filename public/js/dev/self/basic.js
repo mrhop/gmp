24f7af3ec4c32a8425f059f5c5eff5e633645039
mrhop <233402923@qq.com>
2018-05-07 17:44:48 +0800

    update

--- public/js/dev/self/basic.js
+++ public/js/dev/self/basic.js
@@ -2,9 +2,22 @@
  * Created by Donghui Huo on 2016/3/15.
  */
 var $ = jQuery = require('jquery');
+require("flexslider");
 require("bootstrap");
 var utilFun = require("utilFun");
 $(document).ready(function () {
-   //do something
+    if ($("body").attr("id") === 'index') {
+        $('.flexslider').flexslider({
+            fadeFirstSlide: false,
+            controlNav: false,
+            directionNav: true,
+            animationSpeed: 1000,
+            slideshowSpeed: 4000,
+            slideshow: true,
+            animation: "slide",
+            prevText: "",
+            nextText: "",
+        });
+    }
 });
 
