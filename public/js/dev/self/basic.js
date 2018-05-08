324176b9fd16b49810e992a6dac049d5f5d7b745
mrhop <233402923@qq.com>
2018-05-08 17:02:01 +0800

    update

--- public/js/dev/self/basic.js
+++ public/js/dev/self/basic.js
@@ -9,19 +9,17 @@ $(document).ready(function () {
     var bodyId = $("body").attr("id")
     $(".navbar-nav li.active").removeClass("active");
     $(".navbar-nav li." + bodyId + "-li").addClass("active");
-    $(".navbar-nav li." +bodyId + "-parent-li").addClass("active");
-    if (bodyId === 'index') {
-        $('.flexslider').flexslider({
-            fadeFirstSlide: false,
-            controlNav: false,
-            directionNav: true,
-            animationSpeed: 1000,
-            slideshowSpeed: 4000,
-            slideshow: true,
-            animation: "slide",
-            prevText: "",
-            nextText: "",
-        });
-    }
+    $(".navbar-nav li." + bodyId + "-parent-li").addClass("active");
+    $('.flexslider').flexslider({
+        fadeFirstSlide: false,
+        controlNav: false,
+        directionNav: true,
+        animationSpeed: 1000,
+        slideshowSpeed: 4000,
+        slideshow: true,
+        animation: "slide",
+        prevText: "",
+        nextText: "",
+    });
 });
 
