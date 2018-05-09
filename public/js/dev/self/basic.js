818bb4c944a8adbe4b2a66b2e6f8997ab881aa34
mrhop <233402923@qq.com>
2018-05-09 17:18:55 +0800

    update

--- public/js/dev/self/basic.js
+++ public/js/dev/self/basic.js
@@ -3,6 +3,8 @@
  */
 var $ = jQuery = require('jquery');
 require("flexslider");
+require("imageMapResizer");
+require("maphilight");
 require("bootstrap");
 var utilFun = require("utilFun");
 $(document).ready(function () {
@@ -21,5 +23,60 @@ $(document).ready(function () {
         prevText: "",
         nextText: "",
     });
+    if (bodyId === 'layouts') {
+        $('.general img.general-img').maphilight()
+        var flagOpened = false
+        var minWidth = 760
+        $('.general #mapGeneral area').click(function (e) {
+            var curentTarget = $('.flat-1 .subwrapper .' + $(this).attr('id'))
+            if ($(window).outerWidth() > minWidth) {
+                if (!flagOpened) {
+                    curentTarget.css('display', 'block').css('transition', '400ms width linear').css('width', function () {
+                        return $('.flat-1').width() - 382
+                    }).on('webkitTransitionEnd oTransitionEnd transitionend msTransitionEnd', function () {
+                        curentTarget.find('.sub-img').maphilight()
+                        curentTarget.find('map').imageMapResize()
+                    })
+                    flagOpened = true
+                } else {
+                    $('.flat-1 .subwrapper .sub').css('display', 'none')
+                    $('.flat-1 .subwrapper .' + $(this).attr('id')).css('display', 'block').css('width', function () {
+                        return $('.flat-1').width() - 382
+                    })
+                    curentTarget.find('.sub-img').maphilight()
+                    curentTarget.find('map').imageMapResize()
+                }
+            } else {
+                $('.flat-1 .general').css('transition', '400ms width linear').css('width', 0)
+                $('.flat-1 .subwrapper .' + $(this).attr('id')).css('display', 'block').css('transition', '400ms width linear').css('width', function () {
+                    return $('.flat-1').width()
+                }).on('webkitTransitionEnd oTransitionEnd transitionend msTransitionEnd', function () {
+                    curentTarget.find('.sub-img').maphilight()
+                    curentTarget.find('map').imageMapResize()
+                })
+                $('.flat-1 .subwrapper .close-icon').click(function () {
+                    $('.flat-1 .general').css('transition', '400ms width linear').css('width', 382)
+                    $('.flat-1 .subwrapper .sub').css('display', 'none').css('transition', 'none').css('width', 0)
+                })
+            }
+
+            // 给出必须的hide和显示
+            e.preventDefault();
+        })
+        $('.content-section .modal').click(function (e) {
+            if (e.target.classList.contains('modal')) {
+                $('.content-section .modal').css('display', 'none')
+            }
+        })
+        $('.content-section .modal .close-icon').click(function () {
+            $('.content-section .modal').css('display', 'none')
+        })
+        $('.content-section .subwrapper map area').click(function (e) {
+            $('.content-section .modal').css('display', 'flex')
+            $('.content-section .modal img.sub').css('display', 'none')
+            $('.content-section .modal img.' + e.currentTarget.id).css('display', 'block')
+            e.preventDefault()
+        })
+    }
 });
 
